ad2f5dff78fe2d2774017636fab2cf2451c16600
karstensensensen <simonblsoerensen@gmail.com>
2023-09-20 01:06:00 +0200

    Remove unused files

--- test-docker/RunTests.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-import json
-import urllib.request
-from pathlib import Path
-
-print("HELLO WORLD")
-sys.exit(1)
--- test-docker/get_extdir.lua
+++
@@ -1,9 +0,0 @@
--- ASEPRITE_EXTDIR_FILE should be an environment variable, that contains a path to a file, which will contain the aseprite extensions directory,
--- after this script is run.
-print("HELLO WORLD")
-
-for k, v in pairs(app.params) do
-    print(k, v)
-end
-
-io.open(app.params.extdir_file, "w"):write(app.fs.joinPath(app.fs.userConfigPath, "extensions")):close()
