9fb68b4b8f0e655e260f2a15e4e19c7f672857ae
karstensensensen <simonblsoerensen@gmail.com>
2023-09-26 01:56:54 +0200

    Cleanup extension main

--- src/extension/main.lua
+++ src/extension/main.lua
@@ -2,21 +2,8 @@
 --- Aseprite extension for automatic version control of sprites.
 --- 
 
--- function FOO()
--- end
-
--- function TEST()
-
---     for k, v in pairs(debug.getinfo(FOO)) do
---         print(k, v)
---     end
-
--- end
-
--- TEST()
 
 function init(plugin)
-    print("PLUGIN INIT")
 end
 
 function exit(plugin)
