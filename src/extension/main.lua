2cb6857173aeb229883333ba7bfaaadf84cd82d8
karstensensensen <simonblsoerensen@gmail.com>
2023-09-26 14:42:07 +0200

    add debug commands

--- src/extension/main.lua
+++ src/extension/main.lua
@@ -2,8 +2,31 @@
 --- Aseprite extension for automatic version control of sprites.
 --- 
 
+require 'Serializer'
+
 
 function init(plugin)
+    plugin:newCommand{
+        id="store_command",
+        title="Store Sprite",
+        group="sprite_crop",
+        onclick = function()
+            local s_sprite = Serializer:genericSerialize(app.sprite)
+            print(s_sprite)
+            Serializer:store(s_sprite, "Sprite")
+        end
+    }
+
+    plugin:newCommand{
+        id="load_command",
+        title="Load Sprite",
+        group="sprite_crop",
+        onclick = function()
+            local s_sprite = Serializer:load("Sprite")
+            local sprite = Serializer:genericDeserialize(s_sprite)
+            print(sprite)
+        end
+    }
 end
 
 function exit(plugin)
